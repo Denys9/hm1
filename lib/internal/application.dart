582c9fc64aadfa08f0d91ec48f6c1a98b3f86cc5
Denys Lozin <113623082+Denys9@users.noreply.github.com>
2023-05-20 09:56:15 +0300

    Update application.dart
    update

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,29 +1,45 @@
-import 'package:flutter/material.dart';
-
-import '../presentation/home.dart';
-
-class Application extends StatelessWidget {
-  const Application({super.key});
+import 'package:flutter/material.dart'; 
  
-  @override
-  Widget build(BuildContext context) {
-   
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-      primary:const Color.fromARGB(255, 61, 194, 255),
-      secondary: const Color.fromARGB(255, 176, 220, 255),
-    )
-    ),
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 214, 243, 255),
-        appBar: AppBar(
-          title: const Text("My profile"),
-          leading: const Icon(Icons.account_box_outlined)
-        ),
-          body: const Home(),
-            )    
-      ); 
-  }
+import '../presentation/home.dart'; 
+ 
+class Application extends StatelessWidget { 
+  const Application({super.key}); 
+  
+  @override 
+  Widget build(BuildContext context) { 
+    
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false, 
+      theme: ThemeData( 
+    colorScheme: ColorScheme.fromSwatch().copyWith( 
+      primary:const Color.fromARGB(255, 61, 194, 255), 
+      secondary: const Color.fromARGB(255, 176, 220, 255), 
+    ) 
+    ), 
+      home: Scaffold( 
+        backgroundColor: const Color.fromARGB(255, 214, 243, 255), 
+        appBar: AppBar( 
+          title: const Text("My profile"), 
+          leading: const Icon(Icons.account_box_outlined), 
+          backgroundColor: Colors.redAccent, 
+        ), 
+         body: Column( 
+          children: [ 
+            Align( 
+              alignment: Alignment.center, 
+              child: SizedBox( 
+                // width: 200,  
+                height: 200, 
+                child: Image.network( 
+                  'https://www.linkpicture.com/q/деніс.jpg', 
+                  fit: BoxFit.cover, 
+                ), 
+              ), 
+            ), 
+            const Home(), 
+          ], 
+        ), 
+            )     
+      );  
+  } 
 }
